211c178bbd75401ae6e7769cc45a6e760919c05e
Dominik Meyer <meyerdominik@users.noreply.github.com>
2023-02-24 15:14:40 +0100

    Update CAN_A2DP_Sink.ino

--- CAN_A2DP_Sink.ino
+++ CAN_A2DP_Sink.ino
@@ -2,57 +2,95 @@
 #include <ESP32CAN.h>
 #include <CAN_config.h>
 
+// ESP pins https://elektro.turanis.de/html/prj135/index.html
+#define I2S_BCK       GPIO_NUM_26
+#define I2S_WS        GPIO_NUM_25
+#define I2S_DATA_OUT  GPIO_NUM_22
+#define I2S_DATA_IN   I2S_PIN_NO_CHANGE
+#define CAN_TX        GPIO_NUM_5
+#define CAN_RX        GPIO_NUM_4
+
+// a2dp
 BluetoothA2DPSink a2dp_sink;
+const uint8_t myBLDevice = 0xACD6181CED4C;
+
+// can
 CAN_device_t CAN_cfg;
-uint8_t myBLDevice = 0xACD6181CED4C;
-bool OnCoolDown = false;
+bool OnCooldown = false;
+
+// temp
+TaskHandle_t T_HandleTemprature;
+extern uint8_t temprature_sens_read();
+#define TEMP (temprature_sens_read() - 32) / 1.8 // convert to °C
+#define MAXTEMP 100
 
 void setup() {
+  // temp
+  // xTaskCreate(HandleTemprature, "Handle Temprature", 1024, NULL, 0, &T_HandleTemprature); // TODO
+
   // can setup
-  CAN_cfg.speed=CAN_SPEED_100KBPS; // https://bimmerguide.de/bmw-bus-systeme/ K-CAN
-  CAN_cfg.tx_pin_id = GPIO_NUM_5; // TODO 
-  CAN_cfg.rx_pin_id = GPIO_NUM_4; // TODO 
+  CAN_cfg.speed = CAN_SPEED_100KBPS; // https://bimmerguide.de/bmw-bus-systeme/ K-CAN
+  CAN_cfg.tx_pin_id = CAN_TX;
+  CAN_cfg.rx_pin_id = CAN_RX;
   CAN_cfg.rx_queue = xQueueCreate(10, sizeof(CAN_frame_t));
   ESP32Can.CANInit();
   
   // a2dp setup
   i2s_pin_config_t my_pin_config = {
-        .bck_io_num = 26, // TODO 
-        .ws_io_num = 25, // TODO 
-        .data_out_num = 22, // TODO
-        .data_in_num = I2S_PIN_NO_CHANGE
-    };
+    .bck_io_num = I2S_BCK, 
+    .ws_io_num = I2S_WS,
+    .data_out_num = I2S_DATA_OUT,
+    .data_in_num = I2S_DATA_IN
+  };
   a2dp_sink.set_pin_config(my_pin_config);
+  a2dp_sink.set_task_core(2);
   a2dp_sink.start("E87");
-
-  if (a2dp_sink.connect_to(&myBLDevice)) {
-    a2dp_sink.set_volume(255); // Volume to max
+  
+  if (a2dp_sink.connect_to((uint8_t*) myBLDevice)) {
+    a2dp_sink.set_volume(255); // volume to max
   }
 }
 
 void loop() {
   CAN_frame_t rx_frame;
   
-  if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
+  if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS)) {
 
     if (!a2dp_sink.is_connected())
       return;
 
-    // TODO: Add cooldown
-    if (rx_frame.FIR.B.FF == CAN_frame_std && rx_frame.FIR.B.RTR != CAN_RTR) {
-      if (rx_frame.MsgID == 0x1D6) { // https://www.loopybunny.co.uk/CarPC/can/1D6.html & http://www.loopybunny.co.uk/CarPC/k_can.html
-        if (rx_frame.data.u8[0] == 0xC0 and rx_frame.data.u8[1] == 0x0C) {
-          // Continues ping; do nothing
-        } else if (rx_frame.data.u8[0] == 0xE0 and rx_frame.data.u8[1] == 0x0C) { // Up Button
-          a2dp_sink.next();
-        } else if (rx_frame.data.u8[0] == 0xD0 and rx_frame.data.u8[1] == 0x0C) { // Down Button
-          a2dp_sink.previous();
-        } else if (rx_frame.data.u8[0] == 0xC0 and rx_frame.data.u8[1] == 0x0D) { // Voice button
-          a2dp_sink.play();
-        } else if (rx_frame.data.u8[0] == 0xC1 and rx_frame.data.u8[1] == 0x0C) { // Telephone Button
-          a2dp_sink.pause();
-        }
+    if (OnCooldown)
+      return;
+
+    if (rx_frame.FIR.B.FF != CAN_frame_std)
+      return;
+      
+    if (rx_frame.FIR.B.RTR != CAN_RTR)
+      return;
+    
+    if (rx_frame.MsgID == 0x1D6) { // https://www.loopybunny.co.uk/CarPC/can/1D6.html & http://www.loopybunny.co.uk/CarPC/k_can.html
+      if (rx_frame.data.u8[0] == 0xC0 and rx_frame.data.u8[1] == 0x0C) { // continues Ping; nothing is pressed
+        OnCooldown = false;
+      } else if (rx_frame.data.u8[0] == 0xE0 and rx_frame.data.u8[1] == 0x0C) { // up button
+        a2dp_sink.next();
+        OnCooldown = true;
+      } else if (rx_frame.data.u8[0] == 0xD0 and rx_frame.data.u8[1] == 0x0C) { // down button
+        a2dp_sink.previous();
+        OnCooldown = true;
+      } else if (rx_frame.data.u8[0] == 0xC0 and rx_frame.data.u8[1] == 0x0D) { // voice button
+        a2dp_sink.play();
+        OnCooldown = true;
+      } else if (rx_frame.data.u8[0] == 0xC1 and rx_frame.data.u8[1] == 0x0C) { // telephone button
+        a2dp_sink.pause();
+        OnCooldown = true;
       }
     }
+  }
+}
+
+void HandleTemprature() {
+  if (TEMP >= MAXTEMP) {
+    a2dp_sink.disconnect();
+    esp_deep_sleep_start();
   }
 }
