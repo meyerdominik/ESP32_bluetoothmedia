432b41985df22a9a074d8d04bbcac49f612398af
Dominik Meyer <meyerdominik@users.noreply.github.com>
2023-03-04 09:23:15 +0100

    Update CAN_A2DP_Sink.ino

--- CAN_A2DP_Sink.ino
+++ CAN_A2DP_Sink.ino
@@ -1,6 +1,10 @@
+#include <ACAN_ESP32.h>
+#include <ACAN_ESP32_AcceptanceFilters.h>
+#include <ACAN_ESP32_Buffer16.h>
+#include <ACAN_ESP32_CANRegisters.h>
+#include <ACAN_ESP32_Settings.h>
+#include <CANMessage.h>
 #include <BluetoothA2DPSink.h>
-#include <ESP32CAN.h>
-#include <CAN_config.h>
 
 // ESP pins https://elektro.turanis.de/html/prj135/index.html
 #define I2S_BCK       GPIO_NUM_26
@@ -15,10 +19,12 @@ BluetoothA2DPSink a2dp_sink;
 #define ENABLE_A2DP
 
 // can
-CAN_device_t CAN_cfg;
 bool OnCooldown = false;
-#define CAN_SPEED CAN_SPEED_100KBPS // K-CAN: https://bimmerguide.de/bmw-bus-systeme/
+#define CAN_SPEED 100000 // K-CAN: https://bimmerguide.de/bmw-bus-systeme/
 #define ENABLE_CAN
+#ifdef ENABLE_CAN
+// #define DEBUG_CAN
+#endif
 
 void setup() {
   Serial.begin(9600);
@@ -26,11 +32,22 @@ void setup() {
   // can setup
 #ifdef ENABLE_CAN
   Serial.println("Booting CAN");
-  CAN_cfg.speed = CAN_SPEED;
-  CAN_cfg.tx_pin_id = CAN_TX;
-  CAN_cfg.rx_pin_id = CAN_RX;
-  CAN_cfg.rx_queue = xQueueCreate(10, sizeof(CAN_frame_t));
-  ESP32Can.CANInit();
+
+  ACAN_ESP32_Settings settings (CAN_SPEED);
+  settings.mRequestedCANMode = ACAN_ESP32_Settings::ListenOnlyMode;
+  settings.mRxPin = CAN_RX;
+  settings.mTxPin = CAN_TX;
+  
+  const uint32_t errorCode = ACAN_ESP32::can.begin(settings);
+
+#ifdef DEBUG_CAN
+  if (errorCode == 0) {
+    Serial.println ("CAN ok") ;
+  } else {
+    Serial.print ("Error Can: 0x") ;
+    Serial.println (errorCode, HEX) ;
+  }
+#endif
 #endif 
 
   // a2dp setup
@@ -52,51 +69,62 @@ Serial.println("Boot ok");
 
 void loop() {
 #ifdef ENABLE_CAN
-  CAN_frame_t rx_frame;
+  CANMessage frame;
   
-  Serial.print("Waiting on Frame: ");
-  if(xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS)) {
-    Serial.println("found!");
+  if(ACAN_ESP32::can.receive(frame)) {
+
+#ifdef DEBUG_CAN
+    // Print frame
+    Serial.print("New frame with id: 0x");
+    Serial.print(frame.id, HEX);
+    Serial.print(", len: ");
+    Serial.print(frame.len);
+    Serial.print(", data: 0x");
+    for(int i = 0; i < frame.len; i++) {
+      Serial.print(frame.data[i], HEX);
+    }
+    Serial.println("");
+#endif
     
+#ifdef ENABLE_A2DP
     if (!a2dp_sink.is_connected())
       return;
+#endif
 
-    if (OnCooldown)
-      return;
-
-    if (rx_frame.FIR.B.FF != CAN_frame_std)
+    if (frame.ext)
       return;
       
-    if (rx_frame.FIR.B.RTR != CAN_RTR)
-      return;
+    if (frame.rtr)
+      return;  
     
-    if (rx_frame.MsgID == 0x1D6) { // https://www.loopybunny.co.uk/CarPC/can/1D6.html & http://www.loopybunny.co.uk/CarPC/k_can.html
-      if (rx_frame.data.u8[0] == 0xC0 and rx_frame.data.u8[1] == 0x0C) { // continues Ping; nothing is pressed
+    if (frame.id == 0x1D6) { // https://www.loopybunny.co.uk/CarPC/can/1D6.html & http://www.loopybunny.co.uk/CarPC/k_can.html
+      if (frame.len < 2)
+        return;
+
+      if (frame.data[0] == 0xC0 and frame.data[1] == 0x0C) { // continues Ping; nothing is pressed
         OnCooldown = false;
-      } else if (rx_frame.data.u8[0] == 0xE0 and rx_frame.data.u8[1] == 0x0C) { // up button
+      } else if (!OnCooldown && frame.data[0] == 0xE0 and frame.data[1] == 0x0C) { // up button
 #ifdef ENABLE_A2DP
         a2dp_sink.next();
 #endif
         OnCooldown = true;
-      } else if (rx_frame.data.u8[0] == 0xD0 and rx_frame.data.u8[1] == 0x0C) { // down button
+      } else if (!OnCooldown && frame.data[0] == 0xD0 and frame.data[1] == 0x0C) { // down button
 #ifdef ENABLE_A2DP
         a2dp_sink.previous();
 #endif
         OnCooldown = true;
-      } else if (rx_frame.data.u8[0] == 0xC0 and rx_frame.data.u8[1] == 0x0D) { // voice button
+      } else if (!OnCooldown && frame.data[0] == 0xC0 and frame.data[1] == 0x0D) { // voice button
 #ifdef ENABLE_A2DP
         a2dp_sink.play();
 #endif
         OnCooldown = true;
-      } else if (rx_frame.data.u8[0] == 0xC1 and rx_frame.data.u8[1] == 0x0C) { // telephone button
+      } else if (!OnCooldown && frame.data[0] == 0xC1 and frame.data[1] == 0x0C) { // telephone button
 #ifdef ENABLE_A2DP
         a2dp_sink.pause();
 #endif
         OnCooldown = true;
       }
     }
-  } else {
-    Serial.println("not found!");
   }
 #endif
 }
