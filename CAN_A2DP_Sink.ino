326706cf74cb0231dee87d3fde3d92cb88a922d8
Dominik Meyer <meyerdominik@users.noreply.github.com>
2023-04-25 18:15:45 +0200

    Update CAN_A2DP_Sink.ino

--- CAN_A2DP_Sink.ino
+++ CAN_A2DP_Sink.ino
@@ -24,7 +24,7 @@ bool OnCooldown = false;
 #define CAN_SPEED 100000 // K-CAN: https://bimmerguide.de/bmw-bus-systeme/
 #define ENABLE_CAN
 #ifdef ENABLE_CAN
-// #define DEBUG_CAN
+#define DEBUG_CAN
 #endif
 
 // general
@@ -58,13 +58,20 @@ void setup() {
   // a2dp setup
 #ifdef ENABLE_A2DP
   Serial.println("Booting A2DP");
-  i2s_pin_config_t pin_config = { // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/i2s.html
-    .bck_io_num = I2S_BCK, 
-    .ws_io_num = I2S_WS,
-    .data_out_num = I2S_DATA_OUT,
-    .data_in_num = I2S_DATA_IN
-  };
-  a2dp_sink.set_pin_config(pin_config);
+  static i2s_config_t i2s_config = { // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/peripherals/i2s.html
+        .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = 44100, // updated automatically by A2DP
+        .bits_per_sample = (i2s_bits_per_sample_t)32,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = (i2s_comm_format_t) (I2S_COMM_FORMAT_STAND_I2S),
+        .intr_alloc_flags = 0, // default interrupt priority
+        .dma_buf_count = 8,
+        .dma_buf_len = 64,
+        .use_apll = true,
+        .tx_desc_auto_clear = true // avoiding noise in case of data unavailability
+    };
+  a2dp_sink.set_i2s_config(i2s_config);
+  
   a2dp_sink.set_task_core(1);
   a2dp_sink.start(A2DP_SINK_NAME);
 #endif
